16661c3bfefb6b30acf1bb34aa2f9b4e53cdf00b
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-06 11:17:04 +0000

    Chain different methods into one

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
 # Ask user for their name
 name = input("What's your name? ")
 
-# Remove whitespace from str
-name = name.strip()
-
-# Capitalize user's name
-name = name.title()
+# Remove whitespace from str and capitalize user's name
+name = name.strip().title()
 
 # Say hello to user
 print(f"hello, {name}")
