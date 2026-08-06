700b2984cfba6044487580206013fcfe9f8b919d
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-06 11:20:08 +0000

    Chain different methods directly into input

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
 # Ask user for their name
-name = input("What's your name? ")
-
-# Remove whitespace from str and capitalize user's name
-name = name.strip().title()
+name = input("What's your name? ").strip().title()
 
 # Say hello to user
 print(f"hello, {name}")
