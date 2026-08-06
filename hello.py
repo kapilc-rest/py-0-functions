ea00fdf06c09a1cc6f84058cec02391312831f94
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-06 12:51:14 +0000

    Use main function

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
+def main():
+    hello()
+    name = input("What's your name? ")
+    hello(name)
+
 def hello(to="world"):
     print("hello,", to)
 
-hello()
-name = input("What's your name? ")
-hello(name)
+main()
