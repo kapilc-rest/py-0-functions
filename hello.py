76d53fe1a209f74123f25a5c00ff4951f019f5ae
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-06 12:43:13 +0000

    Define new function hello

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
-# Ask user for their name
-name = input("What's your name? ").strip().title()
+def hello():
+    print("hello")
 
-# Split user's name into first name and last name
-first, last = name.split(" ")
-
-# Say hello to user
-print(f"hello, {first}")
+name = input("What's your name? ")
+hello()
+print(name)
